0f3da52fe6211e872730889f21afb6c22e8ac162
Student997 <saurabh@metronlabs.com>
2022-04-25 16:22:41 +0530

    latest2

--- package.sh
+++ package.sh
@@ -14,16 +14,11 @@ rm -rf $PACKAGE_DIR/*
 mkdir -p $PACKAGE_DIR
 
 # Include the HEAD commit id in the package name
-COMMIT_ID=`git rev-parse --short=6 HEAD`
+#COMMIT_ID=`git rev-parse --short=6 HEAD`
 
 ##############################################################################
-# Build the main SCADAfence-Fortinet app
+# Build the main Connector/Integration app
 FULL_APP_FOLDER="Test_Package"
 FULLAPP_DIR=$PACKAGE_DIR/$FULL_APP_FOLDER
 mkdir $FULLAPP_DIR
 cp -R $SRC_DIR/README.md $FULLAPP_DIR/
-
-# Creating a zip
-#cd $PACKAGE_DIR
-#tar -czvf $PACKAGE_DIR/$FULL_APP_FOLDER-${COMMIT_ID}.tgz $FULL_APP_FOLDER
-#rm -rf $FULLAPP_DIR
